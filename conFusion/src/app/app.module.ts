600dd81eee1d85bdcf4869e0fe82ea9a24ac2af8
Yang <gaoyang721@outlook.com>
2018-05-18 08:18:52 +0200

    promotion service

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './/app-routing.module';
+import {PromotionService} from "./services/promotion.service";
 
 
 @NgModule({
@@ -32,7 +33,7 @@ import { AppRoutingModule } from './/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule,
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
