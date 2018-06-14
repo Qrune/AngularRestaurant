0398447ca42fda21a0a5959d2ed34ca7daad8b1f
Yang <gaoyang721@outlook.com>
2018-06-14 21:59:34 +0800

    added observable

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
 @Injectable()
 export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(DISHES),2000);
-    });
+  getDishes(): Observable<Dish[]> {
+    return Observable.of(DISHES).delay(1000);
   }
 
-  getDish(id: number): Promise<Dish> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
-    });
+  getDish(id: number): Observable<Dish> {
+    return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(1000);
   }
 
-  getFeaturedDish(): Promise<Dish> {
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
-    });
-}
+  getFeaturedDish(): Observable<Dish> {
+    return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(1000);
+  }
+  getDishIds(): Observable<number[]> {
+    return Observable.of(DISHES.map(dish => dish.id ));
+  }
 }
--- conFusion/src/app/services/promotion.service.ts
+++ conFusion/src/app/services/promotion.service.ts
@@ -1,28 +1,20 @@
 import { Injectable } from '@angular/core';
 import {PROMOTIONS} from "../shared/promotions";
 import {Promotion} from "../shared/promotion";
-
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/of';
 @Injectable()
 export class PromotionService {
 
   constructor() { }
-  getPromotions(): Promise<Promotion[]>{
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS),2000);
-    })
+  getPromotions():Observable<Promotion[]>{
+    return Observable.of(PROMOTIONS).delay(1000);
   }
-  getPromotion(id: number): Promise<Promotion>{
-    return new Promise( resolve => {
-      setTimeout(()=> resolve(PROMOTIONS.filter((promo)=>(promo.id === id))[0]),2000);
-    })
-
-
+  getPromotion(id: number):Observable<Promotion>{
+    return Observable.of(PROMOTIONS.filter((promo => (promo => (promo.id === id))))[0]).delay(1000);
   }
-  getFeaturedPromotion(): Promise<Promotion>{
-    return new Promise( resolve => {
-      setTimeout(()=>resolve(PROMOTIONS.filter((promo) => (promo.featured))[0]),2000);
-    } )
-      }
-
+  getFeaturedPromotion():Observable<Promotion> {
+    return Observable.of(PROMOTIONS.filter(promo => promo.featured)[0]).delay(1000);
+  }
 
 }
